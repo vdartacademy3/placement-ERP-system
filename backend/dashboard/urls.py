3440eb0f414741e75bb00f51fdeb1934b3d67a69
vdartacademy3 <vdartacademy3@gmail.com>
2026-07-12 20:18:57 -0700

    add

--- backend/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import dashboard_stats
-
-urlpatterns = [
-    path('', dashboard_stats),
-]
--- backend/department/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Department
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display  = ['id', 'name', 'description', 'created_at']
-    search_fields = ['name']
-    ordering      = ['name'] 
